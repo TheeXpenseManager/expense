51aa787c9a652d980a40596720984a27819f6b1d
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2021-10-27 09:37:25 +0530

    Update Landing.js

--- src/screens/Landing/Landing.js
+++ src/screens/Landing/Landing.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Landing.css";
 import logo from "../../media/Logo/logo.png";
-import image from '../../media/Landing/landing-image.svg';
+import image from "../../media/Landing/landing-image.svg";
+import Button from "@mui/material/Button";
+
 const Landing = () => {
   return (
     <section className="SECTION">
@@ -11,21 +13,32 @@ const Landing = () => {
   );
 };
 function Logo() {
-  return (<section>
-  <div className="LOGO">
-    <img src={logo} alt=""></img>
-  </div>
-  <div className="headings"><p className="title">The eXpense Manager</p></div>
-  <div className="description"><p className="description-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum sed commodo magna ac at amet, felis congue imperdiet.</p></div>
-  </section>
-  )
-};
+  return (
+    <section>
+      <div className="LOGO">
+        <img src={logo} alt=""></img>
+      </div>
+      <div className="headings">
+        <p className="title">The eXpense Manager</p>
+      </div>
+      <div className="description">
+        <p className="description-para">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum sed
+          commodo magna ac at amet, felis congue imperdiet.
+        </p>
+      </div>
+      <Button variant="contained" color="success">
+        SignUp for Free
+      </Button>
+    </section>
+  );
+}
 function LandingGreenImg() {
   return (
-  <div className="GreenImg">
-    <img src={image} alt="" className="person-pc"></img>
-  </div>
-  )
-};
+    <div className="GreenImg">
+      <img src={image} alt="" className="person-pc"></img>
+    </div>
+  );
+}
 
 export default Landing;
