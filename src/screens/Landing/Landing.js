b3b49fed9fbf45d337d9fb1813364318be1b3225
puneetpriyadarshi <40725556+puneetpriyadarshi@users.noreply.github.com>
2021-10-27 08:11:33 +0530

    landing page

--- src/screens/Landing/Landing.js
+++ src/screens/Landing/Landing.js
@@ -1,12 +1,30 @@
 import React from "react";
-import PageHeading from "../../components/Basic Components/PageHeading";
-
+import './Landing.css';
+import logo from '../../media/Logo/logo.png';
+import GreenImg from '../../media/Landing/land.svg';
 const Landing = () => {
   return (
-    <div>
-      <PageHeading heading="Landing" />
-    </div>
+    <section className="SECTION">
+      <Logo/>
+      <LandingGreenImg/>
+    </section>
   );
 };
+function Logo(){
+  return(
+    <img src={logo} className="LOGO" alt=""></img>
+  )
+};
+function LandingGreenImg(){
+  return(
+    <img src={GreenImg} className="GreenImg" alt=""></img>
+  )
+}
+function Heading(){
+  return(
+    <div className="heading">
 
+    </div>
+  )
+}
 export default Landing;
