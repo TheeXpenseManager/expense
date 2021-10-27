0a10513de92378a0a1b0496c5634f56a660f416b
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2021-10-27 08:33:06 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import History from "./screens/History/History";
 import Landing from "./screens/Landing/Landing";
--- src/screens/Landing/Landing.js
+++ src/screens/Landing/Landing.js
@@ -1,30 +1,22 @@
 import React from "react";
-import './Landing.css';
-import logo from '../../media/Logo/logo.png';
-import GreenImg from '../../media/Landing/land.svg';
+import "./Landing.css";
+import logo from "../../media/Logo/logo.png";
+import GreenImg from "../../media/Landing/land.svg";
 const Landing = () => {
   return (
     <section className="SECTION">
-      <Logo/>
-      <LandingGreenImg/>
+      <Logo />
+      <LandingGreenImg />
     </section>
   );
 };
-function Logo(){
-  return(
-    <img src={logo} className="LOGO" alt=""></img>
-  )
-};
-function LandingGreenImg(){
-  return(
-    <img src={GreenImg} className="GreenImg" alt=""></img>
-  )
+function Logo() {
+  return <img src={logo} className="LOGO" alt=""></img>;
 }
-function Heading(){
-  return(
-    <div className="heading">
-
-    </div>
-  )
+function LandingGreenImg() {
+  return <div className="GreenImg"></div>;
+}
+function Heading() {
+  return <div className="heading"></div>;
 }
 export default Landing;
