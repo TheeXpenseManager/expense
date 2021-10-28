4833f125f182bda0e4d0c1337781aa402a0f2ea0
MrinalSinha04 <sinhamrinal04@gmail.com>
2021-10-28 14:41:09 +0530

    login-1

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PageHeading from "../../components/Basic Components/PageHeading";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./Login.css" ;
+
 
 const Login = () => {
   return (
     <div>
       <PageHeading heading="Login" />
+      <TextField className="username-email" id="outlined-basic" label="Username or email" variant="outlined" />
+      <TextField className="password" id="outlined-basic" label="password" variant="outlined" />
+      <Button className="login-button" variant="contained">LOGIN</Button>
     </div>
   );
 };
